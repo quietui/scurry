5b0769a4ec178ffd4a52d0a802f7122b0c39df2e
Cory LaViska <cory@abeautifulsite.net>
2024-12-03 19:58:55 -0500

    remove old animations

--- src/animations/fade-in.ts
+++
@@ -1,4 +0,0 @@
-export const fadeIn: Keyframe[] = [
-  { offset: 0, opacity: '0' },
-  { offset: 1, opacity: '1' }
-];
--- src/animations/slide-in-down.ts
+++
@@ -1,4 +0,0 @@
-export const slideInDown: Keyframe[] = [
-  { offset: 0, transform: 'translate3d(0, -100%, 0)', visibility: 'visible' },
-  { offset: 1, transform: 'translate3d(0, 0, 0)' }
-];
