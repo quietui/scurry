b153238072c770bac2cea31c542a55c2b74c8c19
Cory LaViska <cory@abeautifulsite.net>
2025-02-01 22:08:36 -0500

    fix rotate units

--- src/transition/rotate-x.ts
+++ src/transition/rotate-x.ts
@@ -9,13 +9,13 @@ export function rotateX(options?: Partial<QuietAnimationOptions>): QuietTransiti
       keyframes: [
         { opacity: 0, rotate: '-60deg', translate: `${rtl ? 35 : -35}% 0`, scale: 0.85 },
         { opacity: 0.5, rotate: '-20deg', translate: `${rtl ? 15 : -15}% 0`, scale: 0.95 },
-        { opacity: 1, rotate: '0', translate: '0 0', scale: 1 }
+        { opacity: 1, rotate: '0deg', translate: '0 0', scale: 1 }
       ],
       easing: 'cubic-bezier(0.34, 1.56, 0.64, 1)'
     },
     exit: {
       keyframes: [
-        { opacity: 1, rotate: '0', translate: '0 0', scale: 1 },
+        { opacity: 1, rotate: '0deg', translate: '0 0', scale: 1 },
         { opacity: 0.5, rotate: '20deg', translate: `${rtl ? -15 : 15}% 0`, scale: 0.95 },
         { opacity: 0, rotate: '60deg', translate: `${rtl ? -35 : 35}% 0`, scale: 0.85 }
       ],
